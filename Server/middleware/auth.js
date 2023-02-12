40f1e0254ad6b92b361bee95aa754974127fea49
JainishaPathak <jainisha.pathak@dmce.ac.in>
2023-02-12 08:01:39 +0530

    added server.js

--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -4,7 +4,8 @@ export const verifyToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
 
-    const decodedToken = await jwt.verify(token, ENV.JWT_SECRET);
+
+    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = decodedToken;
 
