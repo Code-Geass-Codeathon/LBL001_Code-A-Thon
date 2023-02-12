40f4212172ffa49c0b2072c20766df12c8d867d6
JainishaPathak <jainisha.pathak@dmce.ac.in>
2023-02-12 12:35:19 +0530

    refactored the code and added missing lines

--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,57 @@
-//importing external modules
 import express from "express";
-import morgan from "morgan";
-import cors from "cors";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
-
-//importing internal modules
+import multer from "multer";
+import helmet from "helmet";
+import postRoutes from "./routes/posts.js";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
 import connect from "./database/connect.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
-// MOUDLE SETUP
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
 const app = express();
 app.use(express.json());
-dotenv.config();
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);ended: true ;
 app.use(cors());
 
 // CONNECTING DB WITH APPLICATION
